1063bed65d617e96e007a8ad5c2b18c132f92363
Adam <adam.linn@outlook.com>
2020-12-19 20:19:24 -0600

    Initial setup of Visual Studio project with bare-bones content.

---
+++ CSN.ProjectEulerSharp.UI/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using CSN.ProjectEulerSharp.UI.UserControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSN.ProjectEulerSharp.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ucDetailsTab DetailsTab { get; set; }
+
+        private ucOverviewTab OverviewTab { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeTabs();
+        }
+
+        private void MI_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void InitializeTabs()
+        {
+            DetailsTab = new ucDetailsTab();
+            OverviewTab = new ucOverviewTab();
+
+            tabDetails.Content = DetailsTab;
+            tabOverview.Content = OverviewTab;
+        }
+    }
+}
